84e01dfdfae8492838b7a6c8035aa89911b2816a
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2020-05-02 23:55:27 +0200

    Add AlarmLog and SysLog API controllers

---
+++ API/Controllers/AlarmLogController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("{controller}")]
+    public class AlarmLogController : Controller
+    {
+        AlarmLogLogic alarmLogLogic = new AlarmLogLogic();
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(alarmLogLogic.Get(whereClause:null));
+        }
+        
+        [HttpGet]
+        public IActionResult Get(string monitorId)
+        {
+            return Json(alarmLogLogic.GetByMonitor(monitorId));
+        }
+    }
+}
---
+++ API/Controllers/SysLogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("{controller}")]
+    public class SysLogController : Controller
+    {
+        BusinessLogic.SysLogLogic sysLogLogic = new BusinessLogic.SysLogLogic();
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(sysLogLogic.Get(whereClause:null));
+        }
+    }
+}
