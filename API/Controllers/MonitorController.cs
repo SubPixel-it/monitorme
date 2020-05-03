9847a15501749f59743fd8f1109f5618390ae0a8
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2020-05-03 16:03:25 +0200

    Develop Monitor and MonitorGroups Controllers

--- API/Controllers/MonitorController.cs
+++ API/Controllers/MonitorController.cs
@@ -1,4 +1,5 @@
-﻿using BusinessLogic;
+﻿using System;
+using BusinessLogic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -12,6 +13,102 @@ namespace API.Controllers
         public IActionResult GetAll()
         {
             return Json(monitorLogic.Get(whereClause: null));
+        }
+        
+        [HttpGet]
+        [Route("{monitorId}")]
+        public IActionResult GetById(string monitorId)
+        {
+            return Json(monitorLogic.Get(monitorId));
+        }
+
+        [HttpGet]
+        [Route("group/{monitorGroupId}")]
+        public IActionResult GetByGroup(string monitorGroupId)
+        {
+            return Json(monitorLogic.GetByGroup(monitorGroupId));
+        }
+        
+        [HttpPost]
+        public IActionResult Add()
+        {
+            string name = HttpContext.Request.Form["name"];
+            string maxIntervalString = HttpContext.Request.Form["maxInterval"];
+            string monitorGroupId = HttpContext.Request.Form["groupId"];
+
+            if (String.IsNullOrEmpty(name) ||
+                String.IsNullOrEmpty(maxIntervalString) ||
+                (String.IsNullOrEmpty(monitorGroupId) && monitorGroupId != ""))
+            {
+                return new BadRequestObjectResult("A required parameter is null or empty.")
+                {
+                    StatusCode = 411
+                };
+            }
+
+            bool isInt = int.TryParse(maxIntervalString, out int maxInterval);
+            if (!isInt)
+            {
+                return new BadRequestObjectResult("maxInterval format not valid. Please use integer.")
+                {
+                    StatusCode = 411
+                };
+            }
+
+            Models.Monitor monitor = monitorLogic.Add(name, maxInterval, monitorGroupId == "" ? null : monitorGroupId);
+            if (monitor == null)
+            {
+                return BadRequest("Something went wrong :(");
+            }
+
+            return Json(monitor);
+        }
+        
+        [HttpPatch]
+        [Route("{monitorId}")]
+        public IActionResult Update(string monitorId)
+        {
+            string name = HttpContext.Request.Form["name"];
+            string maxIntervalString = HttpContext.Request.Form["maxInterval"];
+            string monitorGroupId = HttpContext.Request.Form["groupId"];
+            string statusString = HttpContext.Request.Form["status"];
+
+            if (String.IsNullOrEmpty(name) ||
+                String.IsNullOrEmpty(maxIntervalString) ||
+                String.IsNullOrEmpty(statusString) ||
+                (String.IsNullOrEmpty(monitorGroupId) && monitorGroupId != ""))
+            {
+                return new BadRequestObjectResult("A required parameter is null or empty.")
+                {
+                    StatusCode = 411
+                };
+            }
+
+            bool isInt = int.TryParse(maxIntervalString, out int maxInterval);
+            if (!isInt)
+            {
+                return new BadRequestObjectResult("maxInterval format not valid. Please use integer.")
+                {
+                    StatusCode = 411
+                };
+            }
+            isInt = int.TryParse(statusString, out int status);
+            if (!isInt)
+            {
+                return new BadRequestObjectResult("Status format not valid. Please use integer.")
+                {
+                    StatusCode = 411
+                };
+            }
+
+            Models.Monitor monitor = monitorLogic.Update(monitorId, name, maxInterval,
+                monitorGroupId == "" ? null : monitorGroupId, (Models.Monitor.State) status);
+            if (monitor == null)
+            {
+                return BadRequest("Something went wrong :(");
+            }
+
+            return Json(monitor);
         }
     }
 }
--- API/Controllers/MonitorGroupController.cs
+++ API/Controllers/MonitorGroupController.cs
@@ -1,4 +1,5 @@
-﻿using BusinessLogic;
+﻿using System;
+using BusinessLogic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -12,6 +13,81 @@ namespace API.Controllers
         public IActionResult GetAll()
         {
             return Json(monitorGroupLogic.Get(whereClause: null));
+        }
+        
+        [HttpGet]
+        [Route("{monitorGroupId}")]
+        public IActionResult GetById(string monitorGroupId)
+        {
+            return Json(monitorGroupLogic.Get(monitorGroupId));
+        }
+        
+        [HttpPost]
+        public IActionResult Add()
+        {
+            string name = HttpContext.Request.Form["name"];
+            string statusString = HttpContext.Request.Form["status"];
+
+                if (String.IsNullOrEmpty(name) ||
+                String.IsNullOrEmpty(statusString))
+            {
+                return new BadRequestObjectResult("Name or status is null or empty.")
+                {
+                    StatusCode = 411
+                };
+            }
+
+            bool isInt = int.TryParse(statusString, out int status);
+            if (!isInt)
+            {
+                return new BadRequestObjectResult("Status format not valid. Please use integer.")
+                {
+                    StatusCode = 411
+                };
+            }
+
+            Models.MonitorGroup monitorGroup = monitorGroupLogic.Add(name, (Models.Monitor.State) status);
+            if (monitorGroup == null)
+            {
+                return BadRequest("Something went wrong :(");
+            }
+
+            return Json(monitorGroup);
+        }
+        
+        [HttpPatch]
+        [Route("{monitorGroupId}")]
+        public IActionResult Update(string monitorGroupId)
+        {
+            string name = HttpContext.Request.Form["name"];
+            string statusString = HttpContext.Request.Form["status"];
+
+            if (String.IsNullOrEmpty(name) ||
+                String.IsNullOrEmpty(statusString))
+            {
+                return new BadRequestObjectResult("Name or status is null or empty.")
+                {
+                    StatusCode = 411
+                };
+            }
+
+            bool isInt = int.TryParse(statusString, out int status);
+            if (!isInt)
+            {
+                return new BadRequestObjectResult("Status format not valid. Please use integer.")
+                {
+                    StatusCode = 411
+                };
+            }
+
+            Models.MonitorGroup monitorGroup = monitorGroupLogic.Update(monitorGroupId, name,
+                (Models.Monitor.State) status);
+            if (monitorGroup == null)
+            {
+                return BadRequest("Something went wrong :(");
+            }
+
+            return Json(monitorGroup);
         }
     }
 }
